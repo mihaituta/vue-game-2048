397932b609e3c75fa3b8b7b64c16b5ec6c607875
mihaituta <tutamihai@gmail.com>
2022-04-11 09:52:19 +0300

    created score component

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,13 +1,8 @@
 <template>
-  <div class="top-display">
-    <button class="button-new" @click="newGame">New Game</button>
-    <div class="score-container">
-      <div class="score__label">Score</div>
-      <div class="score__numbers">{{ score }}</div>
-    </div>
-  </div>
+  <Score :score="score" @newGame="newGame" />
 
   <h2 class="result" v-if="result !== ''">{{ result }}</h2>
+
   <div id="board" ref="boardRef">
     <div v-for="tile in boardTiles" :class="`tile x${tile}`">
       <div v-show="tile !== 0">{{ tile }}</div>
@@ -17,6 +12,7 @@
 
 <script setup>
 import { computed, onMounted, ref } from 'vue'
+import Score from './Score.vue'
 
 const boardTiles = ref([])
 const score = ref(0)
@@ -232,52 +228,6 @@ onMounted(() => {
 .result {
   margin-bottom: 1rem;
   font-size: 2rem;
-}
-
-.top-display {
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 0.3rem;
-}
-
-.score-container {
-  padding: 0.5rem;
-  margin-bottom: 1.5rem;
-  background-color: $text-primary;
-  border: none;
-  outline: none;
-  font-size: 1.2rem;
-  font-weight: bold;
-  border-radius: 0.3rem;
-  color: hsl(0, 0%, 100%);
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-
-  .score__label {
-    margin-bottom: 0.2rem;
-  }
-  .score__numbers {
-    font-size: 1.5rem;
-  }
-}
-
-.button-new {
-  padding: 0.8rem;
-  margin-bottom: 1.5rem;
-  background-color: $text-primary;
-  border: none;
-  outline: none;
-  font-size: 1.2rem;
-  font-weight: bold;
-  border-radius: 0.3rem;
-  color: hsl(0, 0%, 100%);
-  cursor: pointer;
-  transition: 200ms;
-  &:hover {
-    background-color: hsl(159, 47%, 61%);
-  }
 }
 
 #board {
---
+++ src/components/Score.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="top-display">
+    <button class="btn-new-game" @click="restartGame">New Game</button>
+    <div class="score-container">
+      <div class="score__label">Score</div>
+      <div class="score__numbers">{{ score }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps(['score'])
+const emit = defineEmits(['newGame'])
+const restartGame = () => emit('newGame')
+</script>
+
+<style scoped lang="scss">
+.top-display {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.3rem;
+
+  .btn-new-game {
+    padding: 0.8rem;
+    margin-bottom: 1.5rem;
+    background-color: $text-primary;
+    border: none;
+    outline: none;
+    font-size: 1.2rem;
+    font-weight: bold;
+    border-radius: 0.3rem;
+    color: hsl(0, 0%, 100%);
+    cursor: pointer;
+    transition: 200ms;
+    &:hover {
+      background-color: hsl(159, 47%, 61%);
+    }
+  }
+
+  .score-container {
+    padding: 0.5rem;
+    margin-bottom: 1.5rem;
+    background-color: $text-primary;
+    border: none;
+    outline: none;
+    font-size: 1.2rem;
+    font-weight: bold;
+    border-radius: 0.3rem;
+    color: hsl(0, 0%, 100%);
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+
+    .score__label {
+      margin-bottom: 0.2rem;
+    }
+    .score__numbers {
+      font-size: 1.5rem;
+    }
+  }
+}
+</style>
